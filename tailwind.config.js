bead929aa81f6cb904661f8bc95cebb8d6f3fb7e
Rafael Inácio <rafaelinacio.contato@gmail.com>
2025-02-18 13:33:51 -0300

    :wrench: chore: configure Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Garante suporte a JSX e TSX
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#4D4D56",
+          dark: "#2F2F33",
+        },
+        accent: {
+          light: "#62626C",
+          dark: "#4D4D56",
+        },
+        click: {
+          DEFAULT: "#1267FC",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
